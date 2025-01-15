c4011a106ba2f78f310c0e62ee63bc2aad24744a
ahad hossain <wpahad1997@gmail.com>
2025-01-15 21:35:01 +0600

    lonyo last update

--- assets/js/app.js
+++ assets/js/app.js
@@ -742,3 +742,29 @@ function wowAnimation() {
   });
   wow.init();
 }
+$(window).scroll(reOrder);
+$(window).resize(reOrder);
+$(window).scroll(reOrder);
+$(window).resize(reOrder);
+function reOrder() {
+  var mq = window.matchMedia("(min-width: 992px)");
+  if (mq.matches) {
+    $('.right-child').addClass('customm');
+    $('.right-child h2').text('Scroll');
+    var scroll = $(window).scrollTop(),
+      topContent = $('.one').position().top - 25,
+      sectionHeight = $('.left').height(),
+      rightHeight = $('.right-child').height(),
+      bottomContent = topContent + sectionHeight - rightHeight - 45;
+    if (scroll > topContent && scroll < bottomContent) {
+      $('.customm').removeClass('posAbs').addClass('posFix');
+    } else if (scroll > bottomContent) {
+      $('.customm').removeClass('posFix').addClass('posAbs');
+    } else if (scroll < topContent) {
+      $('.customm').removeClass('posFix');
+    }
+  } else {
+    $('.right-child').removeClass('customm posAbs posFix');
+    $('.right-child h2').text("fixed");
+  }
+}
